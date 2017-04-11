7f2dd9dc0e4cd973c122746823c96e19f142c33c
Montanna Tilton <m.tilton@outlook.com>
2017-04-11 14:03:44 -0700

    Now we have vue, pug, and express installed.
    Baseline

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 6000));
+app.set('port', (process.env.PORT || 4000));
 
 app.use(express.static(__dirname));
 
 // views is directory for all template files
-app.set('views', __dirname);
+app.set('views', "./views");
 app.set('view engine', 'pug');
 
 app.get('/', function(request, response) {
