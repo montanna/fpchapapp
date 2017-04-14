8adbee507d4729ae8bb9a337c8ef390928e65ac7
Montanna Tilton <m.tilton@outlook.com>
2017-04-13 17:00:07 -0700

    gets display name from firebase, login and signup with firebase set up

--- public/js/site.js
+++ public/js/site.js
@@ -1,53 +1,151 @@
-var getPageFromScrollPos = function() {
-    var pos = document.body.scrollTop;
-    var s1 = document.getElementById("section1").getBoundingClientRect().top;
-    var s2 = document.getElementById("section2").getBoundingClientRect().top;
-    if (pos < s2) return "Home";
-}
+ //Globals
+ var curUser;
 
-function initVue() {
+ // Initialize Firebase
+ var config = {
+     apiKey: "AIzaSyAKypRvUnUzE6XmUIXCOIe4Al2x2Xd593s",
+     authDomain: "chaplains-63a1a.firebaseapp.com",
+     databaseURL: "https://chaplains-63a1a.firebaseio.com",
+     projectId: "chaplains-63a1a",
+     storageBucket: "chaplains-63a1a.appspot.com",
+     messagingSenderId: "776082682108"
+ };
+ firebase.initializeApp(config);
 
-    var navHeader = new Vue({
-        el: '#navHeader',
-        data: {
-            page: getPageFromScrollPos()
-        }
-    });
-    var navCollapseToggleHamburger = new Vue({
-        el: '.navCollapseToggleBtn', //the hamburger icon in the nav bar
-        data: {
-            toggleNav: function() {
-                var nav = document.getElementById("navbar");
-                if (nav.classList.contains("collapse")) {
-                    nav.className = "";
-                } else
-                    nav.className = "collapse";
-            }
-        }
-    });
-    var navCollapseToggleWholeBar = new Vue({
-        el: '#navbar', //the whole length of the nav bar
-        data: {
-            toggleNav: function() {
-                var nav = document.getElementById("navbar");
-                if (nav.classList.contains("collapse")) {
-                    nav.className = "";
-                } else
-                    nav.className = "collapse";
-            }
-        }
-    });
-    var userWelcome = new Vue({
-        el: '#userWelcome',
-        data: {
-            user: "Rodney" //TODO: replace with firebase user name variable
-        }
-    })
-}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    //Replaces $(document).ready(){}
-    //because who needs jQuery in 2017
-    initVue();
 
-});
+
+
+ //Initialize the Vue vm
+ var vm = new Vue({
+     el: '.content',
+     created() {
+
+         //DEVELOPMENT ONLY
+         //TODO: remove before publishing!
+         /*
+         firebase.auth().signInWithEmailAndPassword("montanna@mjttech.com", "ChaplainsAreCool2017!")
+             .catch(function(error) {
+                 // Handle Errors here.
+                 var errorCode = error.code;
+                 var errorMessage = error.message;
+                 if (errorCode === 'auth/wrong-password') {
+                     alert('Wrong password.');
+                 } else {
+                     alert(errorMessage);
+                 }
+                 console.log(error);
+             });
+             */
+
+         // find and set user
+         firebase.auth().onAuthStateChanged((user) => {
+
+             if (!user)
+                 return;
+             var dang = document.getElementsByClassName("danger");
+             var passInput = document.getElementById("pass");
+             var pWarning = document.getElementById("warning");
+             pWarning.classList.add("success");
+             vm.warning = "Success! You are now logged in.";
+             for (var i = 0; i < dang.length; i++) {
+                 dang[i].classList.remove("danger");
+
+             }
+
+
+
+             vm.user.uid = user.uid;
+             vm.user.name = user.displayName;
+
+         });
+
+     },
+     data: {
+         user: { name: "", uid: "" },
+         email: "",
+         pass: "",
+         warning: "",
+         messages: [
+             { date: "4/13/2017" },
+             { date: "4/11/2017" },
+
+
+         ]
+     },
+     methods: {
+
+         getPageFromScrollPos: function() {
+             //checks the scroll positition, then sets the label in the nav bar accordingly
+             var pos = document.body.scrollTop;
+             var s1 = document.getElementById("section1").getBoundingClientRect().top;
+             var s2 = document.getElementById("section2").getBoundingClientRect().top;
+             if (pos < s2) return "Home";
+             else return "";
+         },
+         toggleNav: function() {
+             //toggles the collapsed state of the navbar
+             var nav = document.getElementById("navbar");
+             if (nav.classList.contains("collapse")) {
+                 nav.className = "";
+             } else
+                 nav.className = "collapse";
+         },
+         signIn: function() {
+             //sign in with firebase
+             firebase.auth().signInWithEmailAndPassword(vm.email, vm.pass)
+                 .catch(function(error) {
+                     // Handle Errors here.
+                     var errorCode = error.code;
+                     var errorMessage = error.message;
+                     if (errorCode === 'auth/wrong-password') {
+                         var passInput = document.getElementById("pass");
+                         var pWarning = document.getElementById("warning");
+                         passInput.classList.add("danger");
+                         passInput.classList.add("weakPass");
+                         pWarning.classList.remove("success");
+                         pWarning.classList.add("danger");
+                         vm.pass = "";
+                         vm.warning = "Email or password is incorrect.";
+                     } else {
+                         alert(errorMessage);
+                     }
+                 });
+         },
+         signUp: function() {
+             //create a new user in firebase
+             firebase.auth().createUserWithEmailAndPassword(vm.email, vm.pass).catch(function(error) {
+                 // Handle Errors here.
+                 var errorCode = error.code;
+                 var errorMessage = error.message;
+                 console.log(error);
+                 if (errorCode === 'auth/weak-password') {
+                     var passInput = document.getElementById("pass");
+                     var pWarning = document.getElementById("warning");
+                     passInput.classList.add("danger");
+                     passInput.classList.add("weakPass");
+                     pWarning.classList.remove("success");
+                     pWarning.classList.add("danger");
+                     vm.pass = "";
+                     vm.warning = "That password is incorrect.";
+                 }
+             });
+         },
+         closeModal: function() {
+             var modal = document.getElementById("modal");
+             modal.style.display = "none";
+         },
+         showModal: function() {
+             var modal = document.getElementById("modal");
+             modal.style.display = "block";
+         },
+     }
+ })
+
+
+
+ //document.addEventListener("DOMContentLoaded", function(event) {
+ //Replaces $(document).ready(){}
+ //because who needs jQuery in 2017
+
+ //});
