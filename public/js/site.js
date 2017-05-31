3cd053cafeec1e8f58ebd9ce72793ca417241148
Montanna Tilton <m.tilton@outlook.com>
2017-05-31 09:43:02 -0700

    it's been too long since my last commit
    oops

--- public/js/site.js
+++ public/js/site.js
@@ -21,23 +21,6 @@
      el: '.content',
      created() {
 
-         //DEVELOPMENT ONLY
-         //TODO: remove before publishing!
-         /*
-         firebase.auth().signInWithEmailAndPassword("montanna@mjttech.com", "ChaplainsAreCool2017!")
-             .catch(function(error) {
-                 // Handle Errors here.
-                 var errorCode = error.code;
-                 var errorMessage = error.message;
-                 if (errorCode === 'auth/wrong-password') {
-                     alert('Wrong password.');
-                 } else {
-                     alert(errorMessage);
-                 }
-                 console.log(error);
-             });
-             */
-
          // find and set user
          firebase.auth().onAuthStateChanged((user) => {
 
@@ -64,40 +47,31 @@
              var role;
              ref.on('value', function(snapshot) {
                  role = snapshot.val().role;
-                 if (role === "admin")
+                 if (role === "admin") {
                      vm.user.role = "admin";
-                 else
+                 } else
                      vm.user.role = "user";
              });
 
-
-
-
          });
-
 
      },
      data: {
+         page: "Home",
          user: { name: "", uid: "", role: "" },
          email: "",
          pass: "",
          warning: "",
+         selectedReport: { index: 0, class: "", title: "" },
          messages: [
              { date: "4/13/2017" },
              { date: "4/11/2017" },
-
-
+         ],
+         reportTypes: [
+             { index: 0, class: "btnPurple", title: "RISE Report", steps: [""] },
+             { index: 1, class: "btnRed", title: "Domestic Disturbance Report" },
+             { index: 2, class: "btnBlue", title: "Chaplain Activity Report" }
          ]
-     },
-     computed: {
-         getPageFromScrollPos: function() {
-             //checks the scroll positition, then sets the label in the nav bar accordingly
-             var pos = document.body.scrollTop;
-             var s1 = document.getElementById("section1").getBoundingClientRect().top;
-             var s2 = document.getElementById("section2").getBoundingClientRect().top;
-             if (pos < s2) return "Home";
-             else return "";
-         }
      },
      methods: {
          toggleNav: function() {
@@ -166,17 +140,113 @@
              var modal = document.getElementById("modal");
              modal.style.display = "block";
          },
-         startReport: function() {
+         showCard: function() {
+             var card = document.getElementById("card");
+             card.style.left = "0";
+             vm.page = "<small>" + vm.selectedReport.title + "</small>";
+
+         },
+         hideCard: function() {
+             var card = document.getElementById("card");
+             card.style.left = "100vw";
+             vm.page = "Report";
+         },
+         selectReport: function() {
+             var index = event.target.id;
+             vm.selectedReport = vm.reportTypes[index];
+             vm.showCard();
+         },
+         scrollTo: function(event) {
+             //when making a button that triggers a scroll to another element,
+             //the button's id should be the id of the target + "Btn"
+             //ex. the button for scrolling to #map should be #mapBtn
+             //this way the id works like an argument being passed to the function
+             //and we don't have ~16 different functions to do the exact same thing
+             //then we pass the id to the real scroll function hooray
+             event.stopPropagation();
+             var id = event.target.id.slice(0, -3);
+             vm.scroll(id);
+         },
+         scroll: function(id) {
+             var target = document.getElementById(id);
+             var to = target.offsetTop;
+             var scroll = document.body.scrollTop;
+             var diff = to - scroll;
+             var dir = 1; //sugar we're scrolling down
+             if (diff < 0) {
+                 dir = -1; //we need to scroll up instead of down
+                 diff += (2 * diff); //make sure diff is not negative w/crude abs value calculation
+             }
+             if (to >= 50) to -= 50; //account for height of nav bar
+             //attempt at ease in-out like scroll
+             var getAmt = function(scroll) {
+                 //scroll 8px at a time if we're in the first or last 6th of the scroll distance
+                 if ((scroll <= (diff / 6)) || scroll >= ((diff / 6) * 5)) return 8;
+                 //scroll 16px at a time if we're in the first or last 3rd of the scroll distance
+                 else if ((scroll <= (diff / 3)) || scroll >= ((diff / 3) * 2)) return 16;
+                 //scroll 22 px at a time during the middle third of the scroll distance
+                 else return 22;
+                 //these are all arbitrary magic numbers but hey, it works
+             };
+             var scrollInterval = setInterval(function() {
+                 document.body.scrollTop = document.body.scrollTop + (dir * getAmt(scroll));
+                 scroll = document.body.scrollTop;
+
+                 if ((dir === 1) && scroll >= to) {
+                     clearInterval(scrollInterval);
+                     return;
+                 } else if ((dir === -1) && scroll <= to) {
+                     clearInterval(scrollInterval);
+                     return;
+                 }
+
+             }, 10);
 
          }
+
 
      }
  })
 
+ //Watch scroll position
+ document.onscroll = function() {
+     var pos = document.body.scrollTop;
+     var s1 = document.getElementById("section1").offsetTop;
+     var s2 = document.getElementById("section2").offsetTop - 50;
+     var s3 = document.getElementById("section3").offsetTop - 50;
+     if (pos < s2) vm.page = "Home";
+     else if (pos < s3) vm.page = "Report";
+     else vm.page = "Map";
+ }
+
+ //Initialize Google Map plugin only if user is an admin
+ var map;
+
+ function initMap() {
+     map = new google.maps.Map(document.getElementById('map'), {
+         center: { lat: 36.737376, lng: -119.788185 },
+         zoom: 11
+     });
+
+     var geocoder = new google.maps.Geocoder();
+
+     //document.getElementById('submit').addEventListener('click', function() {
+     geocodeAddress(geocoder, map);
+     // });
+ }
 
 
- //document.addEventListener("DOMContentLoaded", function(event) {
- //Replaces $(document).ready(){}
- //because who needs jQuery in 2017
-
- //});
+ function geocodeAddress(geocoder, resultsMap) {
+     var address = "700 Van Ness Ave Fresno CA"
+     geocoder.geocode({ 'address': address }, function(results, status) {
+         if (status === 'OK') {
+             resultsMap.setCenter(results[0].geometry.location);
+             var marker = new google.maps.Marker({
+                 map: resultsMap,
+                 position: results[0].geometry.location
+             });
+         } else {
+             alert('Geocode was not successful for the following reason: ' + status);
+         }
+     });
+ }
